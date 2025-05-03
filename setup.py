32cb55931cdba5855cad543ec72b206d3cd2d232
sasha <99096596+PolskiHacker@users.noreply.github.com>
2025-05-03 22:09:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='win10toast-click',
-    version='0.1.2-custom',
+    version='0.1.2+custom',
     description='Custom fork of win10toast-click with modifications',
     author='Polsi_Hacker',
     url='https://github.com/PolskiHacker/win10_toast_changed.git',
